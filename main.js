f63e324b24f6582ea3f8f50f5ece0a3b739e5d07
Галеев Руслан <ruslan2071@yandex.ru>
2021-11-29 15:37:09 +0500

    commit4

--- main.js
+++
@@ -1,9 +0,0 @@
-var myImage=document.querySelector('img');
-myImage.onclick=function() {
-	var mySrc=myImage.getAttribute('src');
-	if(mySrc==='images/vacuumator1.jpg') {
-	myImage.setAttribute('src', 'images/vacuumator2.jpg');
-	} else{
-		myImage.setAttribute('src','images/vacuumator1.jpg');
-	}
-}
