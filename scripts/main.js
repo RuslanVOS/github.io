33f007f9f92c04ac069a53fce665e1500b87d71c
Галеев Руслан <ruslan2071@yandex.ru>
2021-11-29 15:06:51 +0500

    'commit3'

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,19 @@ myImage.onclick=function() {
 		myImage.setAttribute('src','images/vacuumator1.jpg');
 	}
 }
+var myButton=document.querySelector('button');
+var myHeading=document.querySelector('h1');
+function setUserName() {
+var myName=prompt('Please enter your name');
+localStorage.setItem('name', myName);
+myHeading.textContent='Hello'+myName;
+}
+if (!localStorage.getItem('name')) {
+setUserName();
+} else {
+	var storedName=localStorage.getItem(name);
+	myHeading.textContent='Hello'+storedName;
+}
+myButton.onclick = function() {
+	setUserName();
+}
