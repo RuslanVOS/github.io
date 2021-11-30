b0d5842fd437bc6e751c90819378f183cac0d8ad
Галеев Руслан <ruslan2071@yandex.ru>
2021-11-30 15:59:17 +0500

    commit4

--- scripts/main.js
+++ scripts/main.js
@@ -12,12 +12,12 @@ var myHeading=document.querySelector('h1');
 function setUserName() {
 var myName=prompt('Please enter your name');
 localStorage.setItem('name', myName);
-myHeading.textContent='Hello'+myName;
+myHeading.textContent='Hello '+myName;
 }
-if (!localStorage.getItem('name')) {
+if(!localStorage.getItem('name')) {
 setUserName();
 } else {
-	var storedName=localStorage.getItem(name);
+	var storedName=localStorage.getItem('name');
 	myHeading.textContent='Hello'+storedName;
 }
 myButton.onclick = function() {
